5b2ecebbd0534995bae7a0e8a5f28478299819d6
martinr-max <74423661+martinr-max@users.noreply.github.com>
2020-11-25 14:55:30 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,13 +6,9 @@ const config = {
         databaseURL: 'https://training-app-cf8f3.firebaseio.com>',
         projectId: 'training-app-d460e',
         storageBucket: 'training-app-cf8f3.appspot.com',
-        messagingSenderId: '209321811851'
-      
+        messagingSenderId: '209321811851'     
 }
 
 firebase.initializeApp(config);
-
-
-
 
 export default firebase;

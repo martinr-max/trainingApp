28ccde610e537788aead435d096294ae05ddcd44
martinr-max <74423661+martinr-max@users.noreply.github.com>
2020-11-25 20:56:43 +0200

    Update CurrentTrainingTab.js

--- src/training/current-training/current-training-tab/CurrentTrainingTab.js
+++ src/training/current-training/current-training-tab/CurrentTrainingTab.js
@@ -13,89 +13,95 @@ import CurrentTrainingTimer from '../current-training-timer/Current-training-tim
 
 const CurrentTrainingTab = () => {
 
-	const [startExercise, setStartExercise] = useState(false);
-	const [progress, setProgress] = useState(0);
-	const [exercise, setExercise] = useState('');
-	const [loading, setLoading] = useState(false);
+  const [startExercise, setStartExercise] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [exercise, setExercise] = useState('');
+  const [loading, setLoading] = useState(false);
 
-	const interval = useRef(0);
-	const currentExercise = useRef('');
-	const { uid } = useContext(AuthContext);
-	const history = useHistory();
+  const interval = useRef(0);
+  const currentExercise = useRef('');
+  const {
+    uid
+  } = useContext(AuthContext);
+  const history = useHistory();
 
-	const handleExerciseChange = (event) => {
-		if (event.target.value !== 0) {
-			setExercise(event.target.value);
-		}
-		return;
-	};
+  const handleExerciseChange = (event) => {
+    if (event.target.value !== 0) {
+      setExercise(event.target.value);
+    }
+    return;
+  };
 
-	const CloseChooseExercise = () => {
-		setStartExercise(false);
-	};
+  const CloseChooseExercise = () => {
+    setStartExercise(false);
+  };
 
-	const OpenChoosedExercise = () => {
-		setStartExercise(true);
-	};
+  const OpenChoosedExercise = () => {
+    setStartExercise(true);
+  };
 
-	const sendData = useCallback( async (status) => {
-		clearInterval(interval.current);
-		setProgress(0);
-		setLoading(false);
+  const sendData = useCallback(async (status) => {
+    clearInterval(interval.current);
+    setProgress(0);
+    setLoading(false);
 
-		let data = {
-			...currentExercise.current,
-			id: currentExercise.current.name,
-			duration: currentExercise.current.duration * (progress / 100),
-			calories: currentExercise.current.calories * (progress / 100),
-			date: (new Date())
-				.toLocaleDateString()
-				.split("."),
-			status: status,
-			exUserId: uid
-		};
-		try {
-			let response = await fetch(
-				'https://training-app-d460e.firebaseio.com/exercises.json', {
-					method: "POST",
-					headers: { 'Content-Type': 'application/json' },
-					body: JSON.stringify(data)
-				})
-			response.json();
-		} catch (err) {
-			console.log(err.message);
-		}
-	}, [progress, uid])
+    let data = {
+      ...currentExercise.current,
+      id: currentExercise.current.name,
+      duration: currentExercise.current.duration * (progress / 100),
+      calories: currentExercise.current.calories * (progress / 100),
+      date: (new Date())
+        .toLocaleDateString()
+        .split("."),
+      status: status,
+      exUserId: uid
+    };
+    try {
+      let response = await fetch(
+        'https://training-app-d460e.firebaseio.com/exercises.json', {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+      response.json();
+    } catch (err) {
+      console.log(err.message);
+    }
+  }, [progress, uid])
 
-	useEffect(() => {
-		currentExercise.current = exercises.find(ex => ex.id === exercise);
-		if (currentExercise.current) {
-			let duration = currentExercise.current.duration / 100 * 1000;
-			if (loading) {
-				interval.current = setInterval(() => {
-					setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 1))
-				}, duration);
-			}
-			if (progress >= 100) {		
-				sendData('completed');
-			}
-			return () => clearInterval(interval.current)
-		}
+  useEffect(() => {
+    currentExercise.current = exercises.find(ex => ex.id === exercise);
+    if (currentExercise.current) {
+      let duration = currentExercise.current.duration / 100 * 1000;
+      if (loading) {
+        interval.current = setInterval(() => {
+          setProgress((prevProgress) => (prevProgress >= 100 ? 10 : prevProgress + 1))
+        }, duration);
+      }
+      if (progress >= 100) {
+        sendData('completed');
+      }
+      return () => clearInterval(interval.current)
+    }
 
-	}, [loading, progress, exercise, uid, sendData]);
+  }, [loading, progress, exercise, uid, sendData]);
 
-	const startTraining = () => {
-		setLoading(true);
-	}
+  const startTraining = () => {
+    setLoading(true);
+  }
 
-	const cancelTraining = () => {
-		sendData('cancelled');
-		history.push({ pathname: "/empty" });
-		history.goBack();
-	}
+  const cancelTraining = () => {
+    sendData('cancelled');
+    history.push({
+      pathname: "/empty"
+    });
+    history.goBack();
+  }
 
-	return (
-		<div className="selectEx">
+  return (
+    <div className="selectEx">
         <Button onClick={OpenChoosedExercise} >
           Open the select
         </Button>
@@ -121,7 +127,7 @@ const CurrentTrainingTab = () => {
           startTraining={startTraining}
         />
     </div>
-	);
+  );
 }
 
 export default CurrentTrainingTab;
